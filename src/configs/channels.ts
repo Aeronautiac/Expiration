d6c981efa86a778750b9cc20ef0fb95e02964fc6
Matteo Ibanez <110267915+Aeronautiac@users.noreply.github.com>
2025-10-04 20:01:01 -0400

    fixed kk lounge being kira lounge

--- src/configs/channels.ts
+++ src/configs/channels.ts
@@ -8,7 +8,7 @@ export const channels = {
     courtroom: "1423264458486317098",
     stolenContactLogs: "1423266357507784704",
     tfLounge: "1423266440299155556",
-    kkLounge: "1423266835997917295",
+    kkLounge: "1406124121636737096",
     tfChiefDescription: "1406119727230619773", 
     anonymousCourtroom: "1423266570909651055",
     announcements: "1406115826876284973",
